b6611bad9d3633974e5d59aa15d9a0e2e70a4ce1
Vinay <navik.vinay12@gmail.com>
2022-11-13 10:42:49 +0530

    HeadVsTail shows winner score in %

---
+++ HeadTailPercentage1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic10Programs
+{
+    internal class HeadTailPercentage1
+    {
+        public static void HeadVsTail()
+        {
+            Console.WriteLine("Please enter how many times you want to flip the coin");
+            Random random = new();
+            int n = Convert.ToInt32(Console.ReadLine());
+            int head = 0, tail = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double coinFlipped = random.NextDouble();
+                if (coinFlipped > 0.5)
+                {
+                    tail++;
+                }
+                else
+                {
+                    head++;
+                }
+            }
+            double headPercentage = head * 100 / n;
+            Console.WriteLine($"Head won {head} times,Hence Head scored : {headPercentage}%");
+            double tailPercentage = tail * 100 / n;
+            Console.WriteLine($"Tail won {tail} times,Hence Tail scored : {tailPercentage}%");
+        }
+    }
+}
---
+++ SimplePrograms.cs
@@ -0,0 +1,25 @@
+﻿namespace Basic10Programs
+{
+    internal class SimplePrograms
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("WELCOME!\n" +
+                "Press 1  : Flip Coin and print percentage of Heads and Tails\n" );
+        }
+        public static void Main(string[] args)
+        {
+            Welcome();
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    HeadTailPercentage1.HeadVsTail();
+                    break;
+                default :
+                    Console.WriteLine("Please enter input in ragne 1-10 only");
+                    break;
+            }
+        }
+    }
+}
